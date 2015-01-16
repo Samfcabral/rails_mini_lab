76fb81a736e04fbe9313c49d68abc582c39677cf
Samuel Cabral <samfcabral@gmail.com>
2015-01-16 14:47:35 -0800

    controller setup

--- rails_mini_lab/config/routes.rb
+++ rails_mini_lab/config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+
+  get 'articles/show'
+
+  get 'articles/new'
+
+  get 'articles/edit'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
