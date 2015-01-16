0de739db0413cfc3f15f63d1df8a2f99abdbdacf
Samuel Cabral <samfcabral@gmail.com>
2015-01-16 15:10:50 -0800

    models and associations

---
+++ rails_mini_lab/db/migrate/20150116230111_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :author
+      t.text :content
+
+      t.timestamps null: false
+
+	  t.references :users
+    end
+  end
+end
