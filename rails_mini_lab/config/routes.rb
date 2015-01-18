24ea34cbbea56a4b68912ce04c55d96b08ea01db
Samuel Cabral <samfcabral@gmail.com>
2015-01-17 21:24:44 -0800

    minor change

--- rails_mini_lab/config/routes.rb
+++ rails_mini_lab/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'sessions/new'
 
+  root to: 'articles#index'
+  
+  get '/articles', to: "articles#index"
+
   get 'articles/index'
 
   get 'articles/show'
@@ -17,7 +21,6 @@ Rails.application.routes.draw do
 
   resources :users
 
-  get "/logout", to: "articles"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
