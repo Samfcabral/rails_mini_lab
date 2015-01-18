0c33eabf3502922824f6c76845a6ad7df01b0566
Samuel Cabral <samfcabral@gmail.com>
2015-01-18 15:28:02 -0800

    Controllers and models set up

---
+++ rails_min_lab_take3/db/migrate/20150118230958_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :author
+      t.text :content
+
+      t.timestamps null: false
+      t.references :users
+    end
+  end
+end
